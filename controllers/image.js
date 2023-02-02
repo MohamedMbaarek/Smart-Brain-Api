1c4e7de31467c379cd125090f940d66f3c3738f3
Mohamed Mbarek <72167568+MohamedMbaarek@users.noreply.github.com>
2023-02-02 23:01:09 +0100

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,49 @@
+
+handleAPIcall = (req, res) => {
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": "clarifai",
+            "app_id": "main"
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": req.body.input
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key 33bb76a246934f2db06df627585fa3c1'
+        },
+        body: raw
+    };
+
+    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
+        .then(response => response.json())
+        .then(result => res.json(result))
+        .catch(error => console.log('error', error));
+}
+
+
+handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json("unable to get count"))
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleAPIcall: handleAPIcall
+}
